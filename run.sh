28de1da4dd6a7aad9bd68a4a3b5f230fee0b1e61
Arthur <arthur@corenzan.com>
2017-02-16 14:55:03 -0200

    Remove unecessary stat

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-printf "Domain\tLatency\tSpeed\tLookup\tTime\n"
+printf "Domain Latency Speed Time\n"
 cat URL | while read url; do
   domain=${url%/*}
   domain=${domain#*//}
   latency=$(ping -c4 $domain | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
-  curl -Lo /dev/null -skw "$domain\t$latency\t%{speed_download}\t%{time_namelookup}\t%{time_total}\n" $url
+  curl -Lo /dev/null -skw "$domain $latency %{speed_download} %{time_total}\n" $url
 done | tee -a run.log
